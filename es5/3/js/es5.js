b030db23c5f090d21d681411407cbb18e8df0200
andrea <breezer087@gmail.com>
2021-01-20 15:31:59 +0100

    added 1# es5 exercise

--- es5/3/js/es5.js
+++ es5/3/js/es5.js
@@ -1,3 +1,28 @@
 /* 3. ES5
 Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri */
+
+function arrayOfUniqueNumbers(min, max, length) {
+	var arr = []
+	while(arr.length < length) {
+		var num = Math.floor(Math.random() * (max + 1 - min) + min)
+		if(!arr.includes(num)) {
+			arr.push(num)
+		}
+	}
+	return arr
+}
+
+var arr = arrayOfUniqueNumbers(1, 100, 20)
+console.log(arr)
+
+function arrayMinPosMaxPos(arr, minPos, maxPos) {
+	var newArr = []
+	for (var i = minPos; i <= maxPos; i++) {
+		newArr.push(arr[i])
+	}
+	return newArr
+}
+
+var newArr = arrayMinPosMaxPos(arr, 4, 8)
+console.log(newArr);
