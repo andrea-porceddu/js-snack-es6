adc36aadf32a567ee6781155b139d8aea6707764
andrea-porceddu <breezer087@gmail.com>
2021-01-20 18:17:52 +0100

    update #3 (not #1) es5 exercise after in class room correction by Alfredo/Enrico

--- es5/3/js/es5.js
+++ es5/3/js/es5.js
@@ -25,4 +25,24 @@ function arrayMinPosMaxPos(arr, minPos, maxPos) { // uman positions not machine
 }
 
 var newArr = arrayMinPosMaxPos(arr, 4, 8)
-console.log(newArr);
+console.log(newArr)
+
+// after in classroom correction by Alftedo/Enrico
+function arrayMinPosMaxPos2(arr, minPos, maxPos) {
+  var newArr = []
+  for (var j = 0; j < arr.length; j++) {
+    if (j >= minPos && j <= maxPos) {
+      newArr.push(arr[j]);
+    }
+  }
+  return newArr
+}
+
+var arr2 = arrayOfUniqueNumbers(1, 100, 20)
+console.log(arr2);
+
+var minNum = parseInt(prompt('Inserisci posizione minima'))
+var maxNum = parseInt(prompt('Inserisci posizione massima'))
+
+var newArr2 = arrayMinPosMaxPos2(arr2, minNum, maxNum)
+console.log(newArr2)
